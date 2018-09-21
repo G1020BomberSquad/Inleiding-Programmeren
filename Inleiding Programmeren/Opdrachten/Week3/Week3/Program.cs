4470d12977a88cbbd353a15b9ac6f3304150fc88
G1020BomberSquad <johan01.meer@gmail.com>
2018-09-21 13:33:05 +0200

    fixed array

--- Inleiding Programmeren/Opdrachten/Week3/Week3/Program.cs
+++ Inleiding Programmeren/Opdrachten/Week3/Week3/Program.cs
@@ -10,15 +10,16 @@ namespace Week3
     {
         static void Main(string[] args)
         {
-            //int[] getallen = new int[10];
+            /*
+            int[] getallen = new int[10];
 
-            //for (int i = 0; i < getallen.Length; i++)
-            //{
-            //    getallen[i] = Convert.ToInt32(Console.ReadLine());
-            //}
+            for (int i = 0; i < getallen.Length; i++)
+            {
+                getallen[i] = Convert.ToInt32(Console.ReadLine());
+            }
 
-            //Console.WriteLine("Max: " + getallen.Max());
-            //Console.WriteLine("Min: " + getallen.Min());
+            Console.WriteLine("Max: " + getallen.Max());
+            Console.WriteLine("Min: " + getallen.Min());
 
             List<double> doubles = new List<double>();
             doubles.Add(42.0); // index 0
@@ -67,6 +68,107 @@ namespace Week3
                 }
             }
 
+            string[,] chessBoard = new string[8, 8];
+            */
+
+            
+            Console.WriteLine("Hoe veel getallen wilt u invoeren?");
+            int invoer = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Voert u alstublieft een getal in en druk op enter. Doe dit " + invoer + " keer.");
+            int[] getallen = new int[invoer];
+            double totaal = 0.000;
+            double gemiddelde = 0.000;
+
+            for (int i = 0; i < getallen.Length; i++)
+            {
+                getallen[i] = Convert.ToInt32(Console.ReadLine());
+                totaal = getallen.Sum();
+            }
+            gemiddelde = totaal / invoer;
+            string decimalfix = string.Format("{0:N3}", gemiddelde);
+            Console.WriteLine("Totaal: " + totaal);
+            Console.WriteLine("Gemiddelde: " + decimalfix);
+            
+
+
+            Console.WriteLine("Hoeveel namen wilt u invoeren? Het maximum is 10.");
+            int aantalNamen = Convert.ToInt32(Console.ReadLine());
+
+            if (aantalNamen < 11)
+            {
+                string[] mensen = new string[aantalNamen];
+                Console.WriteLine("Voert u alstublieft een naam in en druk op enter. Doe dit " + aantalNamen + " keer.");
+
+                List<bool> countingBools = new List<bool>();
+                for (int i = 0; i < countingBools.Count; i++)
+                {
+                    countingBools.Add(Convert.ToBoolean(Console.ReadLine()));
+                }
+                // uitleg staat eventueel ook in whatsapp. 
+                // nu nog op kunnen tellen ;)
+
+                for (int i = 0; i < aantalNamen; i++)
+                {
+                    mensen[i] = Console.ReadLine();
+                    if (Console.ReadLine() == "stop")
+                    {
+                        break;
+                    }
+                    if (Console.ReadLine() == "")
+                    {
+                        Console.WriteLine("ERROR: lege invoer niet mogelijk");                        
+                        break;                        
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("FOUT");
+            }
+
+            //string[,] tabel = new string[5, 7]
+            //{
+            //    {"Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag"},
+            //    {"15     ", "12     ", "10      ", "8        ", "9      ", "8       ", "5     "},
+            //    {"6      ", "4      ", "-2      ", "-3       ", "-5     ", "-7      ", "-2    "},
+            //    {"0      ", "1      ", "-2      ", "-4       ", "-3     ", "-8      ", "-9    "},
+            //    {"-11    ", "-8     ", "-5      ", "2        ", "4      ", "5       ", "1     "},
+            //};
+
+            //for (int row = 0; row < 5; row++)
+            //{
+            //    for (int column = 0; column < 7; column++)
+            //    {
+            //        tabel[row, column] = (row).ToString() + (column).ToString();
+            //    }
+            //}
+
+            int[,] data = new int[4, 7]
+            {
+                { 15, 12, 10, 8, 9, 9, 5 },
+                { 6, 4, -2, -3, -5, -7, -2 },
+                { 0, 1, -2, -4, -3, -8, -9 },
+                { -11, -8, -5, 2, 4, 5, 1 },
+            };
+
+            for (int row = 0; row < 4; row++)
+            {
+                for (int column = 0; column < 7; column++)
+                {
+                    Console.Write(data[row, column] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            int sum = 0;
+            for (int j = 0; j < 7; j++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    sum = sum + data[i, j];
+                }
+            }
 
             Console.ReadKey();
         }
